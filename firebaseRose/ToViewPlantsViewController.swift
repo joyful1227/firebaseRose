477d8185343643d503b61e860ed38afae68101d0
joy <le821227@gmail.com>
2019-05-24 22:01:11 +0800

    second version

--- firebaseRose/ToViewPlantsViewController.swift
+++ firebaseRose/ToViewPlantsViewController.swift
@@ -19,73 +19,61 @@ class ToViewPlantsViewController: UIViewController {
     @IBOutlet weak var toViewPageDescription: UILabel!
     @IBOutlet weak var toViewPageGrowdate: UILabel!
     
-    var plants = [QueryDocumentSnapshot]()
-    var planttext: String?
+    //var plants = [QueryDocumentSnapshot]()
+    var plant : QueryDocumentSnapshot?
+    //var planttext: String?
     //var plantt: [String: Any]
     //var test: QueryDocumentSnapshot
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        print("plant",plant)
     }
     
     override func viewWillAppear(_ animated: Bool) {
-        if let planttext = planttext {
-        let firestore = Firestore.firestore()//建立儲藏庫實體
-        firestore.collection("plants").order(by: "update", descending: true).getDocuments { (querySnapshot, error) in
-            if let querySnapshot = querySnapshot {
-                
-                self.plants = querySnapshot.documents
-                print(self.plants)
-                for plant in self.plants {
-                    if plant.data()["name"] as! String == planttext {
-                        
-                        self.toViewPageCategory.text = "種類： \(plant.data()["category"] as? String ?? "")"
-                        self.toViewPageName.text = "名稱： \(plant.data()["name"] as? String ?? "")"
-                        self.toViewPageDescription.text = "\(plant.data()["description"] as? String ?? "")"
-                        //self.toViewPageGrowdate.text = "種植日期： \(String(describing: plant.data()["update"] as? Date))"
-                        //print("種類： \(plant.data()["category"] as? String ?? "")")
-                        //print("名稱： \(plant.data()["name"] as? String ?? "")")
-                        
-                        
-                        if let timeStamp = plant.data()["update"] as? Timestamp {
-                            
-                            let date =  Date(timeIntervalSince1970: TimeInterval(timeStamp.seconds))
-                            let formatter = DateFormatter()
-                            formatter.dateStyle = .short
-                            formatter.timeStyle = .short
-                            self.toViewPageGrowdate.text = "種植日期： \(formatter.string(from: date))"
+        if let plant = plant {
+            self.toViewPageCategory.text = "種類： \(plant.data()["category"] as? String ?? "")"
+            self.toViewPageName.text = "名稱： \(plant.data()["name"] as? String ?? "")"
+            self.toViewPageDescription.text = "\(plant.data()["description"] as? String ?? "")"
+            
+            //如果種植日期是Timestamp 格式，才進行格式化
+            if let timeStamp = plant.data()["update"] as? Timestamp {
+               let date =  Date(timeIntervalSince1970: TimeInterval(timeStamp.seconds))
+               let formatter = DateFormatter()
+               formatter.dateStyle = .short
+               formatter.timeStyle = .short
+               self.toViewPageGrowdate.text = "種植日期： \(formatter.string(from: date))"
+            
+            //透過url取得照片顯示
+            if let imgString: String = plant.data()["imgurl"] as? String {
+//               if let url = URL(string: imgString) {
+//                  let task = URLSession.shared.dataTask(with: url) { (data, response, error) in
+//                      if let data = data {
+//                          DispatchQueue.main.async {                                           self.toViewPageImage.image = UIImage(data: data)
+//                          }
+//
+//                       }
+//                  }
+//                  task.resume()
+//               }
+                getImage(url: URL(string: imgString)) { (image) in
+                    if let image = image {
+                        DispatchQueue.main.async {
+                            self.toViewPageImage.image = image
                         }
-                        
-                        if let imgString: String = plant.data()["imgurl"] as? String {
-                            if let url = URL(string: imgString) {
-                                let task = URLSession.shared.dataTask(with: url) { (data, response, error) in
-                                    if let data = data {
-                                        DispatchQueue.main.async {
-                                            self.toViewPageImage.image = UIImage(data: data)
-                                            }
-                                        
-                                        }
-                                    }
-                                task.resume()
-                                }
-                            
-                            }
-                        }
-                        
-                        //break
-                       
-                        
                     }
                 }
+           }
 
-            }
-          }
+                
+        }else {
+            print("plant not found")
         }
         
 
-    
-
-
+      }
+  }
 
 }
+
+
--- firebaseRose/ViewController.swift
+++ firebaseRose/ViewController.swift
@@ -16,41 +16,54 @@ class ViewController: UIViewController,UICollectionViewDelegate,UICollectionView
     
     let activityIndicatorView = UIActivityIndicatorView(style: .gray)
     var plants = [QueryDocumentSnapshot]()
+    var isfirstGetImage = true
+    
     
     
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        //print("viewDidLoad")
+       
         let image = UIImage()
         self.navigationController?.navigationBar.setBackgroundImage(image, for: .default)
-        //print("viewDidLoad")
-    }
-    
-    
-    override func viewWillAppear(_ animated: Bool) {
         
-        //print("viewWillAppear")
         view.addSubview(activityIndicatorView)
         activityIndicatorView.center = view.center
-        
         activityIndicatorView.startAnimating()
         
+        
         let firestore = Firestore.firestore()//建立儲藏庫實體
-        firestore.collection("plants").order(by: "update", descending: true).getDocuments { (querySnapshot, error) in
-            if let querySnapshot = querySnapshot {
+           firestore.collection("plants").order(by: "update", descending: true).addSnapshotListener { (querySnapshot, error) in
+               if let querySnapshot = querySnapshot {
                 
-                self.plants = querySnapshot.documents
-                print(self.plants)
-                DispatchQueue.main.async {
-                    
-                    self.myCollectionView.reloadData()
-                    self.activityIndicatorView.removeFromSuperview()
-                }
+                   //第一次至firebase取資料
+                    if self.isfirstGetImage == true {
+                       self.plants = querySnapshot.documents
+                       print(self.plants)
+                       
+                       self.isfirstGetImage = false                             //在第一次之後都算非第一次
+                       DispatchQueue.main.async {
+                          self.activityIndicatorView.removeFromSuperview()
+                          self.myCollectionView.reloadData()
+                       }
+                
+                   //非第一次至firebase取資料
+                   }else if self.isfirstGetImage == false {
+                        let documentChange = querySnapshot.documentChanges[0]    //第一個文件
+                        if documentChange.type == .added {                       //如果switch case是新增
+                           self.plants.insert(documentChange.document, at: 0)    //不再重取，而是取第一個放到array中
+                           self.myCollectionView.insertItems(at: [IndexPath(row: 0, section: 0)])
+                        }
+                   }else{
+                       print("self.isfirstGetImage fail")
+                   }
             }
         }
-        
     }
+    
+    
+
     
     
     func numberOfSections(in collectionView: UICollectionView) -> Int {
@@ -72,24 +85,21 @@ class ViewController: UIViewController,UICollectionViewDelegate,UICollectionView
         //print(post.data()["name"] as? String)
         cell.collectViewImage.image = nil
         if let imgString: String = plant.data()["imgurl"] as? String {
-            if let url = URL(string: imgString) {
-                let task = URLSession.shared.dataTask(with: url) { (data, response, error) in
-                    if let data = data {
-                        DispatchQueue.main.async {
-                            if let currentIndexPath = collectionView.indexPath(for: cell),currentIndexPath == indexPath {
-                                cell.collectViewImage.image = UIImage(data: data)
-                            }
-                            
-                        }
+            getImage(url: URL(string: imgString)) { (image) in
+                if let image = image {
+                    DispatchQueue.main.async {
+                        print("URL:\(URL(string: imgString))")
+                        cell.collectViewImage.image = image
                     }
                 }
-                task.resume()
             }
         }
         return cell
         
     }
 
+    
+    
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         if segue.identifier == "GoToAddPlants"  {
              //..
@@ -97,8 +107,9 @@ class ViewController: UIViewController,UICollectionViewDelegate,UICollectionView
             let indexPath = myCollectionView.indexPathsForSelectedItems?.first
             let row = indexPath?.item
             let controller = segue.destination as! ToViewPlantsViewController
-            controller.planttext = plants[(indexPath?.row)!].data()["name"] as? String
-            //controller.test = plants[(indexPath?.row)!]
+            controller.plant = plants[(indexPath?.row)!]
+            
+            //controller.planttext = plants[(indexPath?.row)!].data()["name"] as? String 只傳名字
         }else{
             print("segue fail")
         }
@@ -107,3 +118,20 @@ class ViewController: UIViewController,UICollectionViewDelegate,UICollectionView
 
 }
 
+
+
+
+//取得照片函式，將controller元件有關於completionHandler 作處理
+// @escaping 是於程式執行完畢後執行
+func getImage(url: URL?, completionHandler:  @escaping (UIImage?) -> ()) {
+    if let url = url {
+        let task = URLSession.shared.dataTask(with: url) { (data, response, error) in
+            if let data = data, let image = UIImage(data: data) {
+                completionHandler(image)
+            } else {
+                completionHandler(nil)
+            }
+        }
+        task.resume()
+    }
+}
